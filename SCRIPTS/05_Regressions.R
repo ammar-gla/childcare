1018dad0cbf95e2b0480c5c2f85de4ab7e9a9d3d
ammar-gla <ammar.ljubijankic@london.gov.uk>
2023-03-27 16:21:23 +0100

    subset data to female only for regressions

--- SCRIPTS/05_Regressions.R
+++ SCRIPTS/05_Regressions.R
@@ -9,37 +9,53 @@
 
 # Regress employment on parenthood, sex, age etc.
 ## Note: the format X*Y means the formula includes X+Y+X:Y where the latter is interacted
+# reg_model_vars <- list("uk_wide"=c("parent"),
+#                        "simple"=c("parent","london_resident"),
+#                        "interacted simple"=c("parent*london_resident"),
+#                        "regions"=c("parent","london_resident","manchester_resident","birmingham_resident",
+#                                "SEX_label"),
+#                        "sex simple"=c("parent","london_resident",
+#                                "SEX_label"),
+#                        "sex * London" =c("parent*london_resident*SEX_label"),
+#                        "s*L + age" =c("parent*london_resident*SEX_label","age_group"),
+#                        "s*L + eth" =c("parent*london_resident*SEX_label","ethnicity"),
+#                        "s*L + a + e" =c("parent*london_resident*SEX_label","ethnicity","age_group"),
+#                        "** + child age"=c("parent*london_resident*SEX_label","ethnicity","age_group",
+#                                            "age_child"),
+#                        "** + famtype"=c("parent*london_resident*SEX_label","ethnicity","age_group",
+#                                         "age_child","famtype"),
+#                        "** + religion"=c("parent*london_resident*SEX_label","ethnicity","age_group",
+#                                         "age_child","famtype","RELIG11_label"),
+#                        "** + health"=c("parent*london_resident*SEX_label","ethnicity","age_group",
+#                                          "age_child","famtype","LIMITK_label"),
+#                        "** + num_child"=c("parent*london_resident*SEX_label","ethnicity","age_group",
+#                                           "age_child","famtype","num_children"))
+
+# Simpler models for investigating female London parental gap
 reg_model_vars <- list("uk_wide"=c("parent"),
-                       "simple"=c("parent","london_resident"),
-                       "interacted simple"=c("parent*london_resident"),
-                       "regions"=c("parent","london_resident","manchester_resident","birmingham_resident",
-                               "SEX_label"),
-                       "sex simple"=c("parent","london_resident",
-                               "SEX_label"),
-                       "sex * London" =c("parent*london_resident*SEX_label"),
-                       "s*L + age" =c("parent*london_resident*SEX_label","age_group"),
-                       "s*L + eth" =c("parent*london_resident*SEX_label","ethnicity"),
-                       "s*L + a + e" =c("parent*london_resident*SEX_label","ethnicity","age_group"),
-                       "** + child age"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-                                           "age_child"),
-                       "** + famtype"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-                                        "age_child","famtype"),
-                       "** + religion"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-                                        "age_child","famtype","RELIG11_label"),
-                       "** + health"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-                                         "age_child","famtype","LIMITK_label"),
-                       "** + num_child"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-                                          "age_child","famtype","num_children"))
+                       "London"=c("parent","london_resident"),
+                       "Main simple (**)"=c("parent*london_resident"),
+                       "regions"=c("parent","london_resident","manchester_resident","birmingham_resident"),
+                       "** + age" =c("parent*london_resident","age_group"),
+                       "** + eth" =c("parent*london_resident","ethnicity"),
+                       "** + child age" =c("parent*london_resident","age_child"),
+                       "** + famtype" =c("parent*london_resident","famtype"),
+                       "** + religion" =c("parent*london_resident","RELIG11_label"),
+                       "** + health" =c("parent*london_resident","LIMITK_label"),
+                       "** + num_children" =c("parent*london_resident","num_children"))
 
 # Create empty list to store results
 num_reg_models <- length(reg_model_vars)
 reg_emp_results <- setNames(vector("list", num_reg_models),names(reg_model_vars))
 
+
 # Re-level some of the categorical variables to use as baseline in regressions
+# Restrict the data to only women to make regressions simpler
 survey_2022_reg_data <- update(survey_design_adults[["lfsh_aj22"]],
                                age_group = relevel(factor(age_group),"Aged 25-34"),
                                ethnicity = relevel(factor(ethnicity),"White"),
-                               LIMITK_label = relevel(factor(LIMITK_label),"No"))
+                               LIMITK_label = relevel(factor(LIMITK_label),"No")) %>% 
+  subset(SEX_label=="Female")
 
 # We will only regress the 2022 data for simplicity
 for(i in 1:num_reg_models) {
@@ -71,41 +87,32 @@ export_summs(reg_emp_results,
 
 # Plot coefficients on model
 ## The models to include
-reg_models <- list(reg_emp_results[["sex * London"]],
-                   reg_emp_results[["s*L + age"]],reg_emp_results[["s*L + eth"]],
-                   reg_emp_results[["s*L + a + e"]],reg_emp_results[["** + child age"]],
+reg_models_single <- list(reg_emp_results[["Main simple (**)"]],
+                   reg_emp_results[["** + age"]],reg_emp_results[["** + eth"]],
+                   reg_emp_results[["** + child age"]],
                    reg_emp_results[["** + famtype"]],reg_emp_results[["** + health"]],
-                   reg_emp_results[["** + num_child"]])
+                   reg_emp_results[["** + num_children"]])
 
-reg_models_names <- c("1: London & sex","2: [1] + ethnicity","3: [2] + age",
-                      "4: [3] + child age", "5: [4] + famtype",
-                      "6: [5] + work limiting", "7: [5] + children")
+reg_models_names_single <- c("1: London parents","2: [1] + age",
+                      "3: [1] + ethnicity", "4: [1] + child age","5: [1] + famtype",
+                      "6: [1] + work limiting","7: [1] + #children")
 
-coef_vector_main <- c("Parent"="parent",
-                 "Female parent Londoner"="parent:london_resident:SEX_labelFemale",
-                 "Londoner"="london_resident","Female"="SEX_labelFemale",
-                 "BAME"="ethnicityBAME", "Female parent"="parent:SEX_labelFemale",
-                 "London parent"="parent:london_resident",
-                 "Female Londoner"="london_resident:SEX_labelFemale")
+# coef_vector_main <- c("Parent"="parent",
+#                  "Female parent Londoner"="parent:london_resident:SEX_labelFemale",
+#                  "Londoner"="london_resident","Female"="SEX_labelFemale",
+#                  "BAME"="ethnicityBAME", "Female parent"="parent:SEX_labelFemale",
+#                  "London parent"="parent:london_resident",
+#                  "Female Londoner"="london_resident:SEX_labelFemale")
 
 coef_vector_clean <- c("Parent"="parent",
                       "Londoner"="london_resident",
-                      "Female"="SEX_labelFemale",
-                      "Female parent"="parent:SEX_labelFemale",
-                      "Female parent Londoner"="parent:london_resident:SEX_labelFemale")
+                      "Parent Londoner"="parent:london_resident")
 
-
-coef_plot_main <- plot_summs(reg_models,
-                             model.names = reg_models_names,
-                             coefs=coef_vector_main)
-
-save_GLA_plot("coef_plot_main")
-
-coef_plot_clean <- plot_summs(reg_models,
-                             model.names = reg_models_names,
+coef_plot_single <- plot_summs(reg_models_single,
+                             model.names = reg_models_names_single,
                              coefs=coef_vector_clean)
 
-save_GLA_plot("coef_plot_clean")
+save_GLA_plot("coef_plot_single")
 
 # 
 # survey_2022_reg_data2 <- with(survey_2022_reg_data,
