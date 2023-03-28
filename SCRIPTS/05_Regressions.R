41b65158b95a592526d2b9636a954767609d5a43
ammar-gla <ammar.ljubijankic@london.gov.uk>
2023-03-28 15:35:13 +0100

    subset reg dataset to parents

--- SCRIPTS/05_Regressions.R
+++ SCRIPTS/05_Regressions.R
@@ -9,47 +9,38 @@
 
 # Regress employment on parenthood, sex, age etc.
 ## Note: the format X*Y means the formula includes X+Y+X:Y where the latter is interacted
-# reg_model_vars <- list("uk_wide"=c("parent"),
-#                        "simple"=c("parent","london_resident"),
-#                        "interacted simple"=c("parent*london_resident"),
-#                        "regions"=c("parent","london_resident","manchester_resident","birmingham_resident",
-#                                "SEX_label"),
-#                        "sex simple"=c("parent","london_resident",
-#                                "SEX_label"),
-#                        "sex * London" =c("parent*london_resident*SEX_label"),
-#                        "s*L + age" =c("parent*london_resident*SEX_label","age_group"),
-#                        "s*L + eth" =c("parent*london_resident*SEX_label","ethnicity"),
-#                        "s*L + a + e" =c("parent*london_resident*SEX_label","ethnicity","age_group"),
-#                        "** + child age"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-#                                            "child_age"),
-#                        "** + famtype"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-#                                         "child_age","famtype"),
-#                        "** + religion"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-#                                         "child_age","famtype","RELIG11_label"),
-#                        "** + disability"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-#                                          "child_age","famtype","DISEA_label"),
-#                        "** + num_child"=c("parent*london_resident*SEX_label","ethnicity","age_group",
-#                                           "child_age","famtype","num_children"))
 
-# Simpler models for investigating female London parental gap
-reg_model_vars <- list("uk_wide"=c("parent"),
-                       "London"=c("parent","london_resident"),
-                       "Main simple (**)"=c("parent*london_resident"),
-                       "regions"=c("parent","london_resident","manchester_resident","birmingham_resident"),
-                       "** + age" =c("parent*london_resident","age_group"),
-                       "** + eth" =c("parent*london_resident","ethnicity"),
-                       "** + child age" =c("parent*london_resident","child_age"),
-                       "** + famtype" =c("parent*london_resident","famtype"),
-                       "** + religion" =c("parent*london_resident","RELIG11_label"),
-                       "** + disability" =c("parent*london_resident","DISEA_label"),
-                       "** + num_children" =c("parent*london_resident","num_children"),
-                       "full" =c("parent*london_resident","age_group","ethnicity","famtype","RELIG11_label","num_children","DISEA_label"),
-                       "full + regions" = c("parent*london_resident","parent*manchester_resident","parent*birmingham_resident","age_group","ethnicity",
-                                            "famtype","RELIG11_label","num_children","DISEA_label"))
+
+# Simpler models for investigating female London parental gap, apply on data with female parents only
+
+# reg_model_vars <- list("uk_wide"=c("parent"),
+#                        "London"=c("parent","london_resident"),
+#                        "Main simple (**)"=c("parent*london_resident"),
+#                        "regions"=c("parent","london_resident"),
+#                        "** + age" =c("parent*london_resident","age_group"),
+#                        "** + eth" =c("parent*london_resident","ethnicity"),
+#                        "** + child age" =c("parent*london_resident","child_age"),
+#                        "** + famtype" =c("parent*london_resident","famtype"),
+#                        "** + religion" =c("parent*london_resident","RELIG11_label"),
+#                        "** + disability" =c("parent*london_resident","DISEA_label"),
+#                        "** + num_children" =c("parent*london_resident","num_children"),
+#                        "full" =c("parent*london_resident","age_group","ethnicity","famtype","RELIG11_label","num_children","DISEA_label"))
+
+reg_model_vars <- list("Main simple (**)"=c("london_resident"),
+                       "** + age" =c("london_resident","age_group"),
+                       "** + eth" =c("london_resident","ethnicity"),
+                       "** + child age" =c("london_resident","child_age"),
+                       "** + famtype" =c("london_resident","famtype"),
+                       "** + religion" =c("london_resident","RELIG11_label"),
+                       "** + disability" =c("london_resident","DISEA_label"),
+                       "** + num_children" =c("london_resident","num_children"),
+                       "full" =c("london_resident","age_group","ethnicity","famtype","RELIG11_label","num_children","DISEA_label"))
+
 
 # Create empty list to store results
 num_reg_models <- length(reg_model_vars)
 reg_emp_results <- setNames(vector("list", num_reg_models),names(reg_model_vars))
+reg_emp_results_region <- setNames(vector("list", num_reg_models),names(reg_model_vars))
 
 
 # Re-level some of the categorical variables to use as baseline in regressions
@@ -57,9 +48,9 @@ reg_emp_results <- setNames(vector("list", num_reg_models),names(reg_model_vars)
 survey_2022_reg_data <- update(survey_design_adults[["lfsh_aj22"]],
                                age_group = relevel(factor(age_group),"Aged 25-34"),
                                ethnicity = relevel(factor(ethnicity),"White"),
-                               child_age = relevel(factor(child_age),"More then 4 yrs"),
+                               child_age = relevel(factor(child_age),"4-18 yrs"),
                                DISEA_label = relevel(factor(DISEA_label),"Not Equality Act Disabled")) %>% 
-  subset(SEX_label=="Female")
+  subset(SEX_label=="Female" & parent==1)
 
 # We will only regress the 2022 data for simplicity
 for(i in 1:num_reg_models) {
@@ -68,16 +59,23 @@ for(i in 1:num_reg_models) {
   reg_model <- c(reg_model_vars[[i]])
   
   # Construct a formula object
-  reg_fom <- formula_helper(outcome_var = "employed",
+  reg_form <- formula_helper(outcome_var = "employed",
                             formula_vars = reg_model)
   
   
   reg_emp_results[[i]] <- svyglm(design=survey_2022_reg_data,
-                                 formula = reg_fom)
+                                 formula = reg_form)
+  
+  # Do same regressions adding other regions for interest
+  reg_form_region <- formula_helper(outcome_var = "employed",
+                                    formula_vars = c(reg_model,"manchester_resident","birmingham_resident"))
+  
+  reg_emp_results_region[[i]] <- svyglm(design=survey_2022_reg_data,
+                                 formula = reg_form_region)
   
 }
 
-rm(i)
+rm(i,reg_model,reg_form)
 
 #.............................................................................
 ####  Export results ----
@@ -87,6 +85,10 @@ rm(i)
 export_summs(reg_emp_results,
              to.file = "xlsx",
              file.name = paste0(DATA_OUT,"Regression output 2022.xlsx"))
+
+export_summs(reg_emp_results_region,
+             to.file = "xlsx",
+             file.name = paste0(DATA_OUT,"Regression output REGIONS 2022.xlsx"))
 
 
 # Plot coefficients on model
